dd343ff4ea4369a8638c60647fe978b5d51fef2e
Armaan Vishnoi <armaanvishnoi0065@gmail.com>
2026-06-04 18:08:18 +0530

    fix render typescript build

--- backend/src/middleware/uploadMiddleware.ts
+++ backend/src/middleware/uploadMiddleware.ts
@@ -2,9 +2,9 @@ import multer from "multer";
 import fs from "fs";
 import path from "path";
 
-const uploadPath = path.join(process.cwd(), "uploads/profiles");
+// CREATE UPLOAD PATH
 
-// create folder automatically
+const uploadPath = path.join(process.cwd(), "uploads/profiles");
 
 if (!fs.existsSync(uploadPath)) {
   fs.mkdirSync(uploadPath, {
@@ -12,22 +12,36 @@ if (!fs.existsSync(uploadPath)) {
   });
 }
 
+// STORAGE
+
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
     cb(null, uploadPath);
   },
 
   filename: (req, file, cb) => {
-    cb(
-      null,
-
-      Date.now() + "-" + file.originalname,
-    );
+    cb(null, Date.now() + "-" + file.originalname);
   },
 });
 
-const upload = multer({
-  storage,
-});
+// PROFILE UPLOAD
 
-export default upload;
+export const uploadProfile = multer({
+  storage,
+
+  limits: {
+    fileSize: 5 * 1024 * 1024,
+  },
+
+  fileFilter: (req, file, cb) => {
+    if (
+      file.mimetype === "image/png" ||
+      file.mimetype === "image/jpeg" ||
+      file.mimetype === "image/jpg"
+    ) {
+      cb(null, true);
+    } else {
+      cb(new Error("Only images allowed"));
+    }
+  },
+});
