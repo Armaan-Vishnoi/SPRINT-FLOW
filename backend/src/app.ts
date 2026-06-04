ec4342bb5b7a48c5a48dc4eea0e7a5e6724ad3ef
Armaan Vishnoi <armaanvishnoi0065@gmail.com>
2026-06-04 19:17:56 +0530

    fix render typescript build

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import path from "path";
+
 import authRoutes from "./routes/authRoutes";
 import profileRoutes from "./routes/profileRoutes";
 import projectRoutes from "./routes/projectRoutes";
@@ -11,11 +12,47 @@ import profileManageRoutes from "./routes/profileManageRoutes";
 import commentRoutes from "./routes/commentRoutes";
 import notificationRoutes from "./routes/notificationRoutes";
 import attachmentRoutes from "./routes/attachmentRoutes";
-import { errorHandler } from "./middleware/errorMiddleware";
-import userRoutes from "./routes/userRoutes";
 import dashboardRoutes from "./routes/dashboardRoutes";
-import { limiter, securityHeaders, sanitize } from "./config/security";
+import userRoutes from "./routes/userRoutes";
+
+import { errorHandler } from "./middleware/errorMiddleware";
+
+import { securityHeaders, sanitize } from "./config/security";
+
 const app = express();
+
+// CORS FIRST
+
+app.use(
+  cors({
+    origin: ["http://localhost:5173", "https://sprintflow-plum.vercel.app"],
+
+    credentials: true,
+  }),
+);
+
+// SECURITY
+
+app.use(securityHeaders);
+
+// BODY
+
+app.use(express.json());
+
+// STATIC FILES
+
+app.use(
+  "/uploads",
+
+  express.static(path.join(process.cwd(), "uploads")),
+);
+
+// SANITIZE
+
+app.use(sanitize);
+
+// HEALTH
+
 app.get(
   "/health",
 
@@ -28,48 +65,46 @@ app.get(
   },
 );
 
-app.use(express.json());
-app.use(
-  "/uploads",
+// ROUTES
 
-  express.static(path.join(process.cwd(), "uploads")),
-);
-// SECURITY
-
-app.use(
-  cors({
-    origin: ["http://localhost:5173", "https://your-vercel-url.vercel.app"],
-
-    credentials: true,
-  }),
-);
-app.use(errorHandler);
-app.use(securityHeaders);
-
-//app.use(limiter);
-
-app.use(sanitize);
 app.use("/api/profile", profileRoutes);
+
 app.use("/api/auth", authRoutes);
+
 app.use("/api/projects", projectRoutes);
+
 app.use("/api/sprints", sprintRoutes);
+
 app.use("/api/tasks", taskRoutes);
+
 app.use("/api/worklogs", workLogRoutes);
+
 app.use("/api/user/profile", profileManageRoutes);
+
 app.use("/api/comments", commentRoutes);
+
 app.use("/api/notifications", notificationRoutes);
+
 app.use("/api/attachments", attachmentRoutes);
+
 app.use("/api/dashboard", dashboardRoutes);
-app.get("/", (req, res) => {
-  res.json({
-    success: true,
-    message: "SprintFlow API Running",
-  });
-});
 
-app.use(
-  "/api/users",
+app.use("/api/users", userRoutes);
 
-  userRoutes,
+app.get(
+  "/",
+
+  (req, res) => {
+    res.json({
+      success: true,
+
+      message: "SprintFlow API Running",
+    });
+  },
 );
+
+// ERROR HANDLER ALWAYS LAST
+
+app.use(errorHandler);
+
 export default app;
