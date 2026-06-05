46477f2e6ef3fba796cf759638316a2826ab124f
Armaan Vishnoi <armaanvishnoi0065@gmail.com>
2026-06-05 12:23:18 +0530

    fix vite production build

--- backend/src/services/emailService.ts
+++ backend/src/services/emailService.ts
@@ -1,7 +1,11 @@
 import nodemailer from "nodemailer";
 
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.gmail.com",
+
+  port: 587,
+
+  secure: false,
 
   auth: {
     user: process.env.EMAIL_USER,
@@ -12,11 +16,13 @@ const transporter = nodemailer.createTransport({
 
 export const sendEmail = async (
   to: string,
+
   subject: string,
+
   message: string,
 ) => {
   await transporter.sendMail({
-    from: process.env.EMAIL_USER,
+    from: `"SprintFlow" <${process.env.EMAIL_USER}>`,
 
     to,
 
@@ -24,22 +30,16 @@ export const sendEmail = async (
 
     html: `
 
-<div>
+      <div>
 
-<h2>
-SprintFlow Notification
-</h2>
+        <h2>SprintFlow Notification</h2>
 
+        <p>${message}</p>
 
-<p>
+      </div>
 
-${message}
-
-</p>
-
-
-</div>
-
-`,
+    `,
   });
+
+  console.log("MAIL SENT TO:", to);
 };
